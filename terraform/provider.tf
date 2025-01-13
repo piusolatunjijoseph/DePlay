3b66207563e5839e30dbf57cd7709abb58595c50
etapius <eta.pius-olatunji@smg.com>
2025-01-12 19:21:02 -0700

    clear warnings

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,6 +2,19 @@ provider "azurerm" {
   features {}
 }
 
+│ terraform {
+│   backend "local" {}
+│ }
+
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.0" # Adjust version as per your requirements
+    }
+  }
+}
+
 provider "github" {
   owner = var.github_owner
   token = var.github_token
