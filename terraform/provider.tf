e956e2378f20669e57cc0d6d7b52089e249888b4
etapius <eta.pius-olatunji@smg.com>
2025-01-13 07:32:16 -0700

    provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,7 +1,3 @@
-provider "azurerm" {
-  features {}
-}
-
 terraform {
   required_providers {
     github = {
@@ -9,6 +5,11 @@ terraform {
       version = "~> 5.0" # Adjust version as per your requirements
     }
   }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "b9c4d03f-47e6-4875-92bf-4a77de1c09ee"
 }
 
 provider "github" {
