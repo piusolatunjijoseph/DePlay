a388b3227f800302b5b2783b0af89963a1657aba
etapius <eta.pius-olatunji@smg.com>
2025-01-12 18:42:52 -0700

    fmt

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,10 +1,10 @@
 locals {
-  prefix = "${var.product_name}"
+  prefix = var.product_name
 
   tags = {
-    repository     = local.prefix
-    repository_url = "https://github.com/piusolatunjijoseph/${var.product_name}"
-    terraform-managed   = true
+    repository        = local.prefix
+    repository_url    = "https://github.com/piusolatunjijoseph/${var.product_name}"
+    terraform-managed = true
   }
 
 }
--- terraform/storage_account.tf
+++ terraform/storage_account.tf
@@ -9,11 +9,11 @@ resource "azurerm_storage_account" "deplay_storage" {
   location                 = azurerm_resource_group.deplay_rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  min_tls_version = "TLS1_2"
+  min_tls_version          = "TLS1_2"
 }
 
 resource "azurerm_storage_container" "deplay_container" {
   name                  = "fortknoxx"
-  storage_account_id  = azurerm_storage_account.deplay_storage.id
+  storage_account_id    = azurerm_storage_account.deplay_storage.id
   container_access_type = "private" # Options: "private", "blob", or "container"
 }
